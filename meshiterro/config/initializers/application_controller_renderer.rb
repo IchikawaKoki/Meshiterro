1acdb155238dabb27cfc2102702e8573aacbd5df
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2020-12-16 14:52:54 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '668b021c6b5758a1b240ddf1e80852369cab43ac913857f87e221c632d6424e20ad9002db8482c3346a03a2670d35cb0470a5a288f4e8a4f0789dd709fcb194a'
